c8a0b9bba689031ad2b41499d6438310ff6ac37e
EvanD1st <jamesevan393@gmail.com>
2026-07-20 17:40:32 +0100

    Configure settings for Render production

--- PORTFOLIO/settings.py
+++ PORTFOLIO/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -120,3 +120,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "my_portfolio"/ "static",
 ]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
